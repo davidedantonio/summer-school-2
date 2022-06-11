c94bb6b431e56bbe0b33f1f359e78daffb36cf63
Davide D'Antonio <davide.dantonio@webeetle.com>
2022-06-11 10:49:48 +0200

    enh

--- js/35.js
+++ js/35.js
@@ -2,6 +2,17 @@ function introduceYourself () {
   console.log(`My name is ${this.name}`)
 }
 
-introduceYourself();
-// My name is
+const person1 = { 
+  name: 'Paul',
+  introduceYourself: introduceYourself
+};
+
+const person2 = {
+  name: 'Steve',
+  introduceYourself: introduceYourself
+};
+
+person1.introduceYourself();
+person2.introduceYourself();
+
 
